b9b49dccd640a90a4e52e8ae4eab158c15b54bf8
Christian Ing <cisunardi1@sheffield.ac.uk>
2018-05-23 22:41:34 +0700

    Styled PersonalDetail component

--- components/CompleteProfile/PersonalDetail/PersonalDetail.js
+++ components/CompleteProfile/PersonalDetail/PersonalDetail.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
-import { Text, View, Button } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View, Button, ScrollView, TextInput } from 'react-native';
+import { H3, Label, Input, Item , Form} from 'native-base';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import Material from 'react-native-vector-icons/MaterialIcons';
 import styles from './Stylesheet';
+import commonStyles from '../../../common/CommonStyleSheet';
+import { scale } from 'react-native-size-matters';
 
 class PersonalDetail extends Component {
   static navigationOptions = ({ navigation, navigationOptions }) => {
@@ -14,10 +19,109 @@ class PersonalDetail extends Component {
     }
   };
 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      fname: '',
+      lname: '',
+      location: '',
+      subjects: '',
+      summary: '',
+      lname: '',
+      errors: {},
+      disableSubmit: false,
+      loading: false,
+    };
+    // this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+
+  }
+
   render() {
+    // const { errors } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text> Complete Your Profile </Text>
+        <ScrollView>
+          <View style={styles.centerTitle}>
+            <H3 style={[styles.title, commonStyles.boldText]}>Complete Your Profile</H3>
+          </View>
+          
+          <View style={styles.journey}>
+            <View style={{alignItems: 'center', justifyContent:'center', width: 60, zIndex: 2}}>
+              <View style={{backgroundColor: 'white'}}>
+                <Ionic name="ios-checkmark-circle-outline" size={30} color={'#00b16e'} />
+              </View>
+              <Text style={[commonStyles.boldText, {fontSize: 10, color: '#00b16e', marginTop: 5, letterSpacing: 1}]}>PERSONAL</Text>
+            </View>
+            <View style={{alignItems: 'center', justifyContent:'center', width: 60, marginLeft: scale(50), marginRight: scale(50), zIndex: 2}}>
+              <View style={{backgroundColor: 'white'}}>
+                <Ionic name="ios-checkmark-circle-outline" size={30} color={'#cdccd8'} />
+              </View>
+              <Text style={[commonStyles.boldText, {fontSize: 10, color: '#cdccd8', marginTop: 5, letterSpacing: 1}]}>EXP</Text>
+            </View>
+            <View style={{alignItems: 'center', justifyContent:'center', width: 60, zIndex: 2}}>
+              <View style={{backgroundColor: 'white'}}>
+                <Ionic name="ios-checkmark-circle-outline" size={30} color={'#cdccd8'} />
+              </View>
+              <Text style={[commonStyles.boldText, {fontSize: 10, color: '#cdccd8', marginTop: 5, letterSpacing: 1}]}>SCHEDULE</Text>
+            </View>
+            <View style={{position: 'absolute', left:85, top:15, zIndex: 0, flexDirection:'row', }}>
+              <View style={[styles.hr, {backgroundColor: '#cdccd8'}]}></View>
+              <View style={[styles.hr, {marginLeft: 15}]}></View>
+            </View>
+          </View>
+
+          <View style={{marginTop: 20, paddingLeft: 20, paddingRight: 20, alignItems: 'center',justifyContent: 'center',}}>
+            <View style={commonStyles.itemWrapper}>
+              <Item style={commonStyles.customItem} stackedLabel>
+                <Label style={commonStyles.formLabel}>First Name</Label>
+                <TextInput
+                  underlineColorAndroid='transparent'
+                  style={[commonStyles.formInput]} 
+                  onChangeText={(fname) => this.setState({fname})}
+                  value={this.state.fname}
+                  autoCapitalize = 'none'
+                />
+              </Item>
+            </View>
+          </View>
+
+          <View style={{marginTop: 20, paddingLeft: 20, paddingRight: 20, alignItems: 'center',justifyContent: 'center',}}>
+            <View style={commonStyles.itemWrapper}>
+              <Item style={commonStyles.customItem} stackedLabel>
+                <Label style={commonStyles.formLabel}>Last Name</Label>
+                <TextInput
+                  underlineColorAndroid='transparent'
+                  style={[commonStyles.formInput]} 
+                  onChangeText={(lname) => this.setState({lname})}
+                  value={this.state.lname}
+                  autoCapitalize = 'none'
+                />
+              </Item>
+            </View>
+          </View>
+
+          <View style={{marginTop: 20, paddingLeft: 20, paddingRight: 20, alignItems: 'center',justifyContent: 'center',}}>
+            <View style={commonStyles.itemWrapper}>
+              <Item style={commonStyles.customItem} stackedLabel>
+                <Label style={commonStyles.formLabel}>Location</Label>
+                <TextInput
+                  underlineColorAndroid='transparent'
+                  style={[commonStyles.formInput]} 
+                  onChangeText={(location) => this.setState({location})}
+                  value={this.state.location}
+                  autoCapitalize = 'none'
+                />
+              </Item>
+            </View>
+          </View>
+        </ScrollView>
+        <View style={{alignItems: 'center', justifyContent: 'center',}}>
+          <Text>SUBMIT</Text>
+        </View>
       </View>
     )
   }
--- components/CompleteProfile/PersonalDetail/Stylesheet.js
+++ components/CompleteProfile/PersonalDetail/Stylesheet.js
@@ -3,8 +3,30 @@ import { ScaledSheet } from 'react-native-size-matters';
 export default ScaledSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  centerTitle: {
+    marginTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff',
+  },
+  title: {
+    color: '#606060',
+    letterSpacing: 1,
+  },
+  journey: {
+    marginTop: '15@s',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 50, paddingRight: 50,
+    position: 'relative',
+  },
+
+  hr: {
+    width: '90@s',
+    height: 2,
+    backgroundColor: '#cdccd8',
+    zIndex: 0,
   },
 });
